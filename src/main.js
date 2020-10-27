8e124f9d550207025e4fbc4bf8498ac4748c82dc
Dong-hue <2321402206@qq.com>
2020-10-27 18:53:20 +0800

    项目初始化，引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 // 引入全局样式
 import './styles/index.less'
+// 引入vant
+import vant from 'vant'
+import 'vant/lib/index.css'
+// 导入自适配插件
+import 'amfe-flexible'
 Vue.config.productionTip = false
-
+Vue.use(vant)
 new Vue({
   router,
   store,
